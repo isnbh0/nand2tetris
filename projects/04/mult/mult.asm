4b8d548f9e181afb59c4928b20cddbcf6258b242
John Choi <isnbh0@gmail.com>
2021-02-14 20:59:04 +0900

    WIP project 04: mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,37 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// R2 = 0; i = 0
+@R2
+M=0
+@i
+M=0
+
+(LOOP)
+// branch: if i gte R1, goto END
+@i
+D=M
+@R1
+D=D-M
+@END
+D;JGE
+
+// R2 = R2 + R0
+@R0
+D=M
+@R2
+M=M+D
+
+// i++
+@i
+M=M+1
+
+// goto LOOP
+@LOOP
+0;JMP
+
+// end
+(END)
+@END
+0;JMP
